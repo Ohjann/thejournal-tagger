77573c12d0896fd474d6f59712130dfeb0e7d5e1
Eoghan Hynes <eoghan@hyn.es>
2018-07-06 21:28:07 +0100

    Appending to DOM

--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("comment-7350977"));
+const comments = document.querySelectorAll(".comment");
+
+[...comments].forEach(comment => {
+  const id = `comment-tag-${comment.id}`;
+  const mount = document.createElement("div");
+  mount.id = id;
+  const refNode = comment.querySelector(".author");
+  console.log(`Inserting ${id} as child of`, refNode);
+  refNode.appendChild(mount);
+  ReactDOM.render(<App />, document.getElementById(id));
+});
 registerServiceWorker();
