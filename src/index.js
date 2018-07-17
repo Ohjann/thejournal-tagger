f3c9103a867bb204e98071011c49454cb6f92abb
Eoghan Hynes <eoghan@hyn.es>
2018-07-17 19:40:32 +0100

    Simplify build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import Form from "./Components/Form";
 import registerServiceWorker from "./registerServiceWorker";
 
