6811bc91f83c30427ec4aa4d241b26fa39229978
Eoghan Hynes <eoghan@thejournal.ie>
2018-07-19 18:45:59 +0100

    setTimeout hack

--- src/index.js
+++ src/index.js
@@ -6,26 +6,30 @@ import registerServiceWorker from "./registerServiceWorker";
 
 const comments = document.querySelectorAll(".comment");
 
-browser.runtime
-  .sendMessage({
-    request: "requestLabels"
-  })
-  .then(
-    labels => {
-      [...comments].forEach(comment => {
-        const id = `comment-tag-${comment.id}`;
-        const mount = document.createElement("div");
-        mount.id = id;
-        const refNode = comment.querySelector(".author");
-        refNode.appendChild(mount);
-        ReactDOM.render(
-          <Form commentId={id} label={labels.id} />,
-          document.getElementById(id)
-        );
-      });
-    },
-    error => {
-      console.log(error);
-    }
-  );
+// Ensure background script is loaded first (can't find alternative in docs)
+setTimeout(() => {
+  browser.runtime
+    .sendMessage({
+      request: "requestLabels"
+    })
+    .then(
+      labels => {
+        console.log(labels);
+        [...comments].forEach(comment => {
+          const id = `comment-tag-${comment.id}`;
+          const mount = document.createElement("div");
+          mount.id = id;
+          const refNode = comment.querySelector(".author");
+          refNode.appendChild(mount);
+          ReactDOM.render(
+            <Form commentId={id} label={labels.id} />,
+            document.getElementById(id)
+          );
+        });
+      },
+      error => {
+        console.log(error);
+      }
+    );
+}, 1000);
 registerServiceWorker();
